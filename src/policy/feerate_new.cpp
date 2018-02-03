a9f741b5e100ed1f9d336c0c990f6da22fb381ff
sonofcraig69 <34755215+sonofcraig69@users.noreply.github.com>
2018-02-03 20:51:28 +0000

    Started coding structure for implementation of developer's fee
    Added getDevFeeRate() and getDevFeeAmount()

--- src/policy/feerate_new.cpp
+++ src/policy/feerate_new.cpp
@@ -1,1 +1,16 @@
 
+/** coding this shit in C, no idea if it works in C++ */
+
+/** return multiplier for developer feerate */ 
+int getDevFeeRate() {
+  double feeRate = 0.1; // 10% developer fee
+  return feeRate;
+}
+
+/** return amount to be paid as developer's fee given 
+number of coins mined nCoins */
+int getDevFeeAmount(double nCoins) {
+  double multiplier = getDevFeeRate();
+  return multiplier*nCoins;
+}
+
